d4e622b8c002dc662fd44a46775c57f0978245e6
Abhishek <abhisheksarakanam@attentioun.com>
2022-05-18 10:24:39 +0000

    logs routed in frontend start script

--- Frontend/RunFrontendScript.sh
+++ Frontend/RunFrontendScript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd Frontend/
-serve -n build -l 3001 &
+cd /home/ec2-user/deployment/Frontend
+serve -n build -l 3001 > /home/ec2-user/deploymentlogs/frontenddeployment-$current_time.out 2>/home/ec2-user/deploymentlogs/frontenddeployment-$current_time.err &
