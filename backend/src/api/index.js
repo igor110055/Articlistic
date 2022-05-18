a8906bc863839aeee1f51e348f4fc3234d87a46f
YashDixit <yash@attentioun.com>
2022-05-18 08:33:10 +0000

    Squashed commit of the following
    commit 020ced06 
    Author: 1pervysage0 <msyed7008@gmail.com> 
    Date: Wed May 18 2022 14:01:10 GMT+0530 (India Standard Time) 
    
        ATT074:(fix)resolve merge conflicts

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { parseArray } = require('../utils/parameterStore/paramStore');
 
 var environVarArray = ['SENTRY_URI', 'ATT_AWS_ACCESS_ID', 'ATT_AWS_REGION', 'ATT_AWS_S3_BUCKET_ARTICLES',
@@ -44,32 +43,3 @@ async function x() {
 }
 
 x(); 
-=======
-const app = require('./app');
-var config = require('../../config');
-const logger = require('../utils/logger/index')
-const http = require('http');
-const start = require('./start');
-const {
-    MDB
-} = require('../db/mongo/client');
-const {
-    RDB
-} = require('../db/redis/client');
-
-
-let env = config.environment;
-console.log(env);
-let port = config.hosting.port || 3000;
-let host = config.hosting.host || '0.0.0.0';
-
-const server = http.createServer(app);
-
-// start(server);
-
-server.listen(port, async () => {
-    await MDB.getClient();
-    await RDB.getClient();
-    logger.info("Server is up and running at " + host + " and port: " + port + " at Environment: " + env);
-});
->>>>>>> origin/develop
