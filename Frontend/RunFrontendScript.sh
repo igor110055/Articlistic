56fb8f9a4da4c02821a7a84c1d499974ea7562fc
Abhishek <abhisheksarakanam@attentioun.com>
2022-05-23 07:10:05 +0000

    Debug

--- Frontend/RunFrontendScript.sh
+++ Frontend/RunFrontendScript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/deployment/Frontend
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 serve -n build -l 3001 > /home/ec2-user/deploymentlogs/frontenddeployment-$current_time.out 2>/home/ec2-user/deploymentlogs/frontenddeployment-$current_time.err &
