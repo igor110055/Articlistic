456c49e35afadeb33fc2fd4eb0a126811a3df4ac
Abhishek <abhisheksarakanam@attentioun.com>
2022-05-23 07:29:16 +0000

    :)

--- backend/RunBackendScript.sh
+++ backend/RunBackendScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/deployment/backend/src/api
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-node index.js > /home/ec2-user/deployment-$current_time.out 2>/home/ec2-user/deployment-$current_time.err &
+node index.js > /home/ec2-user/deploymentlogs/deployment-$current_time.out 2>/home/ec2-user/deploymentlogs/deployment-$current_time.err &
